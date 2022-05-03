37fd528b7409660607f759ca921fef37ec9e602d
Amro Abdalla <wed.kom@hotmail.com>
2022-05-03 16:21:24 -0500

    intial commit

---
+++ src/com/devmountain/farm/Main.java
@@ -0,0 +1,7 @@
+package com.devmountain.farm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Instantiate objects here!");
+    }
+}
---
+++ src/com/devmountain/farm/Melon.java
@@ -0,0 +1,28 @@
+package com.devmountain.farm;
+
+public class Melon {
+
+	public static void main(String[] args) {
+		System.out.println("melon defined");
+	}
+
+    MelonType melonType;
+    int shapeRating;
+    int colorRating;
+    int field;
+    String harvester;
+
+	public Melon(MelonType melonType,
+                 int shapeRating,
+                 int colorRating,
+                 int field,
+                 String harvester) {
+		this.melonType = melonType;
+		this.shapeRating = shapeRating;
+		this.colorRating = colorRating;
+		this.field = field;
+		this.harvester = harvester;
+	}
+
+}
+
